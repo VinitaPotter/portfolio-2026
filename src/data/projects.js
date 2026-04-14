fc8b9e4336c1ff3e502e2f22cf49d443e19d2155
VinitaPotter <vinitavkumbhar@gmail.com>
2026-04-14 16:27:35 +0530

    feat: new project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,18 +1,26 @@
 import sudokuImg from "@/assets/images/projects/sudoku-screenshot.png";
 import ticTacToeImg from "@/assets/images/projects/tic-tak-toe-screenshot.png";
+import veganPalateImg from "@/assets/images/projects/vegan-palate.png";
 export const projects = [
-    {
-        name: "Sudoku",
-        image: sudokuImg,
-        tech: "ReactJS",
-        description: "",
-        link: "https://vinitapotter.github.io/sudoku"
-    },
-    {
-        name: "Tic-Tac-Toe",
-        image: ticTacToeImg,
-        tech: "ReactJS",
-        description: "",
-        link: "vinitapotter.github.io/react-tic-tac-toe/"
-    },
+  {
+    name: "Vegan Palate",
+    image: veganPalateImg,
+    tech: "Typescript, ReactJs",
+    description: "",
+    link: "https://vinitapotter.github.io/vegan-palate/",
+  },
+  {
+    name: "Sudoku",
+    image: sudokuImg,
+    tech: "ReactJS",
+    description: "",
+    link: "https://vinitapotter.github.io/sudoku",
+  },
+  {
+    name: "Tic-Tac-Toe",
+    image: ticTacToeImg,
+    tech: "ReactJS",
+    description: "",
+    link: "vinitapotter.github.io/react-tic-tac-toe/",
+  },
 ];
